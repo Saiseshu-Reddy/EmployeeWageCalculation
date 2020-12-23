1efeeb3b65ee7639b9b618e97bdbb4a546b5bb15
Saiseshu Reddy <saiseshureddy.isanaka@gmail.com>
2020-12-23 12:29:23 +0530

    Employee Attendance user case

--- empWageCalculation.sh
+++ empWageCalculation.sh
@@ -2,4 +2,14 @@
 
 echo "Welcome to Employee Wage Calculation Program"
 
+#Employee attendace check user case
+var=$(($RANDOM%2))
+
+if [ $var -eq 1 ]
+then
+	echo "Employee is Present"
+else
+	echo "Employee is Absent"
+fi
+
 
