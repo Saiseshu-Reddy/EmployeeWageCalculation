fac3e71d65b9fedb00a114a534194ec39f6993cf
Saiseshu Reddy <saiseshureddy.isanaka@gmail.com>
2020-12-23 12:34:34 +0530

    Employee Daily Wage Calculation User Case

--- empWageCalculation.sh
+++ empWageCalculation.sh
@@ -2,12 +2,16 @@
 
 echo "Welcome to Employee Wage Calculation Program"
 
+WAGEPERHR=20
+FULLDAYHR=8
+
 #Employee attendace check user case
 var=$(($RANDOM%2))
 
 if [ $var -eq 1 ]
 then
-	echo "Employee is Present"
+	dailyWage=$(($WAGEPERHR*$FULLDAYHR))	#Calculates daily wage of employee if he is present
+	echo "The Daily Wage of Employee is: $dailyWage"
 else
 	echo "Employee is Absent"
 fi
